03513594e702f53de5cf9796a62910cf8c368301
toilavic <t18042k@gmail.com>
2021-05-04 02:38:56 +0300

    ex3 ^ ex5

--- app/src/main/java/com/example/chat/MainActivity.kt
+++ app/src/main/java/com/example/chat/MainActivity.kt
@@ -1,28 +1,49 @@
 package com.example.chat
 
 import android.content.Context
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.LocaleList
+import android.text.InputType
 import android.util.Log
 import android.view.KeyEvent
-import android.view.inputmethod.InputMethod
+import android.view.Menu
+import android.view.MenuItem
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 class MainActivity : AppCompatActivity() {
+    private val TAG: String = MainActivity::class.java.name
     private lateinit var messages: ArrayList<String>
     private lateinit var database : DatabaseReference
     private lateinit var edMessage: EditText
     private lateinit var rcMessageList: RecyclerView
+    private lateinit var auth: FirebaseAuth
+    private var currentUser: FirebaseUser?= null
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -30,7 +51,9 @@ class MainActivity : AppCompatActivity() {
         edMessage = findViewById(R.id.messageText)
         rcMessageList = findViewById(R.id.messageList)
         database = Firebase.database.reference
-        messages = arrayListOf()
+        auth = Firebase.auth
+
+        messages = arrayListOf<String>()
 
         edMessage.setOnKeyListener { v, keyCode, event ->
             if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
@@ -62,14 +85,65 @@ class MainActivity : AppCompatActivity() {
         rcMessageList.adapter = MyAdapter(messages)
     }
 
+    override fun onStart() {
+        super.onStart()
+        currentUser = auth.currentUser
+        if (currentUser == null) loginDialog()
+    }
+
+    fun loginDialog() {
+        val builder = AlertDialog.Builder(this)
+
+        with(builder) {
+            setTitle("Login")
+            val linearLayout: LinearLayout = LinearLayout(this@MainActivity)
+            linearLayout.orientation = LinearLayout.VERTICAL
+
+            val inputEmail: EditText = EditText(this@MainActivity)
+            inputEmail.inputType =
+                    InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS or InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
+            inputEmail.hint = "Enter email"
+            linearLayout.addView(inputEmail)
+
+            val inputPassword: EditText = EditText(this@MainActivity)
+            inputPassword.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS or InputType.TYPE_TEXT_VARIATION_PASSWORD
+            inputPassword.hint = "Enter password"
+            linearLayout.addView(inputPassword)
+            builder.setView(linearLayout)
+
+            builder.setPositiveButton("OK") {dialog, which ->
+                login(inputEmail.text.toString(), inputPassword.text.toString())
+            }.show()
+        }
+    }
+
+    private fun login(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Log.d(TAG, "signInWithEmail: Successfully!")
+                        Toast.makeText(baseContext, "signInWithEmail: Successfully!", Toast.LENGTH_SHORT).show()
+                        currentUser = auth.currentUser
+                    }
+                    else {
+                        Log.w(TAG, "signInWithEmail: Fail!", task.exception)
+                        Toast.makeText(baseContext, "Authentication failed", Toast.LENGTH_SHORT).show()
+                        loginDialog()
+                    }
+                }
+    }
 
 
-    private fun addMessage(){
-        val newMessage = edMessage.text.toString()
-        messages.add(newMessage)
-        database.child("message").setValue(messages)
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun addMessage() {
+        val formatter: DateTimeFormatter = DateTimeFormatter. ofPattern("dd.MM.yyyy HH:mm")
+        val newMessage: Message = Message(edMessage.text.toString(), currentUser?.email.toString(),formatter.format(LocalDateTime.now()))
+        messages.add(newMessage.toString())
+        database.child("messages").setValue(messages)
         edMessage.setText("")
         closeKeyBoard()
+        rcMessageList.smoothScrollToPosition(rcMessageList.adapter!!.itemCount -1)
     }
 
     private fun closeKeyBoard() {
---
+++ app/src/main/java/com/example/chat/Message.kt
@@ -0,0 +1,13 @@
+package com.example.chat
+
+data class Message (val message: String, val author: String?, val time: String) {
+    companion object {
+        fun from(map: HashMap<String, String>) = object {
+            val author by map
+            val time by map
+            val message by map
+
+            val data = Message(message, author, time)
+        }.data
+    }
+}
